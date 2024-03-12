618bce221c2eb249ad01d81e5d420ed7ca988fff
buysobad <163165543+buysobad@users.noreply.github.com>
2024-03-12 23:13:25 +0700

    Add files via upload

---
+++ buysosad.js
@@ -0,0 +1,6 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Không học mà đòi có 9đ 10đ á?? có cái đầu buồi nhé! Done mẹo!");
+
+     // Chuyển hướng đến YouTube
+  window.location.href = "https://rr1---sn-o097znz7.googlevideo.com/videoplayback?expire=1710281143&ei=V33wZbWVFOu9sfIPnMamgA4&ip=2600%3A3c01%3A%3Af03c%3A94ff%3Afebc%3A5f5c&id=o-ACJjb_NhFXA39wzxK8-Gv2W8DQOrq0xEsIvYQsKy0P6D&itag=22&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=7c&mm=31%2C29&mn=sn-o097znz7%2Csn-n4v7snse&ms=au%2Crdu&mv=m&mvi=1&pl=32&initcwndbps=1336250&siu=1&vprv=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=212.091&lmt=1706254927528878&mt=1710259038&fvip=2&fexp=24007246&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Csiu%2Cvprv%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AJfQdSswRQIgIM2Sp-lDKKVYglj_-4Ro69HZ4pZ_fGnYCir3pV6R1NkCIQDCcY8NZF_2dqqFciJI4cKwiMBNKB78svFEhPEMcOKPLw%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=APTiJQcwRgIhAJXwTWdHzqsYKBF7UFVvG6YwEecwcX0KQju0Cff2uLMrAiEAr5Oh1CoeQvdigwbWw3BvH361afHhpyVs6AhQmrJog74%3D";
+});
